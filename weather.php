37021f6d018dc097de8654de76fdf2bd6e040297
Darren Hankins <darren@adrnln.com>
2016-04-07 14:38:35 -0600

    Added an API key from openweather.com

--- weather.php
+++ weather.php
@@ -1,4 +1,9 @@
-<?php include('include.php'); ?>
+<?php
+
+include('config.inc');
+include('include.php');
+
+?>
 
 
 <!-- <form method="post">
@@ -12,7 +17,8 @@
  // if($_SERVER['REQUEST_METHOD'] == "POST"){
     //$url = "http://api.openweathermap.org/data/2.5/forecast?q=".urlencode($_POST['city'])."&mode=json&appid=2de143494c0b295cca9337e1e96b00e0&units=imperial";
     //$url = "http://api.openweathermap.org/data/2.5/weather?q=".urlencode($_POST['city'])."&mode=json&appid=2de143494c0b295cca9337e1e96b00e0&units=imperial";
-	   $url = "http://api.openweathermap.org/data/2.5/weather?lat=".$_GET['lat']."&lon=".$_GET['lon']."&mode=json&appid=2de143494c0b295cca9337e1e96b00e0&units=imperial";
+	   $url = "http://api.openweathermap.org/data/2.5/weather?lat=".$_GET['lat']."&lon=".$_GET['lon'].
+		 "&mode=json&appid=".$apikey."&units=imperial";
     $response = request($url,null, "GET");
     //print "<pre>".print_r($response->list[0]->main->temp, true)."</pre>";
     print "Longitude = ".$response->coord->lon."<br>";
